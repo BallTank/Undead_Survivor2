935378aa7e2db4a516e935634787b38e71ca0b92
YoungDogg <leejonghwo@gmail.com>
2025-11-07 10:04:13 +0900

    [Refactor] Replace characterId magic numbers to characterType
    기존에 0, 1, 2 등으로 하드코딩된 캐릭터ID를 characterType enum으로 교체합니다.
    1. 가독성 향상: id==0 보다 characterId == characterType.Boy 가 더 명확합니다.
    2. 타입 안정성확보: 유효하지 않은 정수값이 할당되어 발생하는 IndexOutOfRangeException같은 버그를 차단함.
    
    주요 변경사항
    GameManager.cs
     if(System.Enum.IsDefined(typeof(characterType), id))
    구문을 사용하여 UI 버튼 등에서 int값을 받아도 안전하게 캐스팅 하도록 추가.
    
    Player.cs
    애니메이터 배열에 접근하기위해 명시적 형변환  사용 (int)GameManager.instance.characterId
    
    Character.cs
    각종 스탯 프로퍼티가 매직넘버 대신 enum을 직접 비교하도록 수정

--- Assets/Undead Survivor/Code/Player.cs
+++ Assets/Undead Survivor/Code/Player.cs
@@ -31,7 +31,7 @@ public class Player : MonoBehaviour
     private void OnEnable()
     {
         speed *= Character.Speed;
-        anim.runtimeAnimatorController = animCon[GameManager.instance.playerId];
+        anim.runtimeAnimatorController = animCon[(int)GameManager.instance.characterId];
     }
 
     private void Update()
